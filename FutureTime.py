924f7ead53f84063c8388f3fc07492d884610073
samykasongo-sk <skikangala@unomaha.edu>
2026-02-02 00:16:32 +0000

    done sam

--- FutureTime.py
+++ FutureTime.py
@@ -16,13 +16,15 @@ def main():
 
   #TODO:
   #Ask user for hours
+  add_hours = int(input("Enter hours to add: "))
   #Ask user for minutes
-
+  add_minutes = int(input("Enter minutes to add: "))
   #Calculate the time after the user-supplied time has passed.
-
+  future_time = now + datetime.timedelta(hours=add_hours, minutes=add_minutes)
   #Do not use any if statements in calculating the time.
 
   #Output the future time in standard format "HH:MM"
+  print("Future time will be:", future_time.strftime("%H:%M"))
 
 
 if __name__ == '__main__':
--- Magic8Ball.py
+++ Magic8Ball.py
@@ -10,9 +10,28 @@ def main():
   #Create a list of your responses.
   print("Magic 8 Ball")
   #Prompt the user for their question.
+   responses = [
+        "Yes, it is",
+        "It is certain.",
+        "Without a doubt.",
+        "You may rely on it.",
+        "Ask again later.",
+        "Better not tell you now.",
+        "Cannot predict now.",
+        "Don't count on it.",
+        "My reply is no.",
+        "Very doubtful."
+    ]
 
+  question = input("Ask me a question (preferably a yes/no question):  ").strip()
   #Answer question randomly with one of the options from your earlier list.
+  answer = random.choice(responses)
 
 
+
+  print(f"The Magic 8 Ball says: {answer}")
+
+
+ 
 if __name__ == '__main__':
   main()
